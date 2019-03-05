26c52595ff46bd3d33cfe7c5a6cab84ee97ccaee
hrbrmstr <bob@rud.is>
2019-03-05 10:41:02 -0500

    R package repo initialization complete

---
+++ R/hdrs-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/hdrs>
+#' - BugReports: <https://gitlab.com/hrbrmstr/hdrs/issues>
+#' 
+#' @md
+#' @name hdrs
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
